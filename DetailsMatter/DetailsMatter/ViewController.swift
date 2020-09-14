fcafaba9f7cc31b53de16f20151a60d2a54b441a
Tales Conrado <talesmateuscc@gmail.com>
2020-09-14 09:16:23 -0300

    initializes project

---
+++ DetailsMatter/DetailsMatter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DetailsMatter
+//
+//  Created by Tales Conrado on 14/09/20.
+//  Copyright © 2020 Tales Conrado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
