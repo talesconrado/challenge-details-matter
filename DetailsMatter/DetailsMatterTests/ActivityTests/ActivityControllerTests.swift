39a24720cd33a126372b67c056ef44d8179c5b7b
Tales Conrado <talesmateuscc@gmail.com>
2020-09-24 12:10:54 -0300

    adds tests for activity controller

---
+++ DetailsMatter/DetailsMatterTests/ActivityTests/ActivityControllerTests.swift
@@ -0,0 +1,53 @@
+//
+//  ActivityControllerTests.swift
+//  DetailsMatterTests
+//
+//  Created by Tales Conrado on 24/09/20.
+//  Copyright © 2020 Tales Conrado. All rights reserved.
+//
+
+import XCTest
+@testable import DetailsMatter
+
+class ActivityControllerTests: XCTestCase {
+    var sut: ActivityController!
+
+    override func setUp() {
+        sut = ActivityController()
+    }
+
+    func test_titleLoaded_onAppear_isNovaAtividade() {
+        //when
+        sut.viewDidLoad()
+        //then
+        XCTAssertEqual(sut.title, "Nova Atividade")
+    }
+
+    func test_detailsViewDelegate_OnCategoryChange_isEqual() {
+        let detailsView = DetailsView(size: 3, type: .categoryDetails)
+        detailsView.delegate = sut
+
+        //when
+        detailsView.selectedOption = 1
+        let selectedOnController = sut.selectedCategory
+
+        //then
+        XCTAssertEqual(detailsView.selectedOption, selectedOnController)
+    }
+
+    func test_detailsViewDelegate_OnRepeatChange_isEqual() {
+        let detailsView = DetailsView(size: 3, type: .repeatDetails)
+        detailsView.delegate = sut
+
+        //when
+        detailsView.selectedOption = 2
+        let selectedOnController = sut.selectedRepeat
+
+        //then
+        XCTAssertEqual(detailsView.selectedOption, selectedOnController)
+    }
+
+    override func tearDown() {
+        sut = nil
+    }
+}
